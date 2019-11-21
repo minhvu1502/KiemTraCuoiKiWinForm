27ae7f8b77cb053e4bc32dad1ba3d8eb16c863c8
minhvu1502 <43747199+minhvu1502@users.noreply.github.com>
2019-11-21 21:52:07 +0700

    add test kiem tra

---
+++ TestKiemTra/TestKiemTra/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace TestKiemTra
+{
+    partial class frm_TestKiemTra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_MCV = new System.Windows.Forms.TextBox();
+            this.txt_TenCV = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_dong = new System.Windows.Forms.Button();
+            this.btn_them = new System.Windows.Forms.Button();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_sua = new System.Windows.Forms.Button();
+            this.btn_excel = new System.Windows.Forms.Button();
+            this.btn_boqua = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Luong = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dlgSave = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_MCV
+            // 
+            this.txt_MCV.Location = new System.Drawing.Point(114, 24);
+            this.txt_MCV.Name = "txt_MCV";
+            this.txt_MCV.Size = new System.Drawing.Size(238, 20);
+            this.txt_MCV.TabIndex = 12;
+            // 
+            // txt_TenCV
+            // 
+            this.txt_TenCV.Location = new System.Drawing.Point(114, 59);
+            this.txt_TenCV.Name = "txt_TenCV";
+            this.txt_TenCV.Size = new System.Drawing.Size(238, 20);
+            this.txt_TenCV.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Mã CV:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Tên Công Việc";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(45, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(498, 194);
+            this.dataGridView1.TabIndex = 17;
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // btn_dong
+            // 
+            this.btn_dong.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_dong.Location = new System.Drawing.Point(581, 391);
+            this.btn_dong.Name = "btn_dong";
+            this.btn_dong.Size = new System.Drawing.Size(83, 36);
+            this.btn_dong.TabIndex = 23;
+            this.btn_dong.Text = "Đóng";
+            this.btn_dong.UseVisualStyleBackColor = true;
+            this.btn_dong.Click += new System.EventHandler(this.btn_dong_Click);
+            // 
+            // btn_them
+            // 
+            this.btn_them.Location = new System.Drawing.Point(136, 391);
+            this.btn_them.Name = "btn_them";
+            this.btn_them.Size = new System.Drawing.Size(83, 36);
+            this.btn_them.TabIndex = 18;
+            this.btn_them.Text = "Thêm";
+            this.btn_them.UseVisualStyleBackColor = true;
+            this.btn_them.Click += new System.EventHandler(this.btn_them_Click);
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Location = new System.Drawing.Point(225, 391);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(83, 36);
+            this.btn_Xoa.TabIndex = 19;
+            this.btn_Xoa.Text = "Xóa";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            this.btn_Xoa.Click += new System.EventHandler(this.btn_Xoa_Click);
+            // 
+            // btn_sua
+            // 
+            this.btn_sua.Location = new System.Drawing.Point(314, 391);
+            this.btn_sua.Name = "btn_sua";
+            this.btn_sua.Size = new System.Drawing.Size(83, 36);
+            this.btn_sua.TabIndex = 20;
+            this.btn_sua.Text = "Sửa";
+            this.btn_sua.UseVisualStyleBackColor = true;
+            this.btn_sua.Click += new System.EventHandler(this.btn_sua_Click);
+            // 
+            // btn_excel
+            // 
+            this.btn_excel.Location = new System.Drawing.Point(403, 391);
+            this.btn_excel.Name = "btn_excel";
+            this.btn_excel.Size = new System.Drawing.Size(83, 36);
+            this.btn_excel.TabIndex = 21;
+            this.btn_excel.Text = "Export";
+            this.btn_excel.UseVisualStyleBackColor = true;
+            this.btn_excel.Click += new System.EventHandler(this.btn_excel_Click);
+            // 
+            // btn_boqua
+            // 
+            this.btn_boqua.Location = new System.Drawing.Point(492, 391);
+            this.btn_boqua.Name = "btn_boqua";
+            this.btn_boqua.Size = new System.Drawing.Size(83, 36);
+            this.btn_boqua.TabIndex = 22;
+            this.btn_boqua.Text = "Làm Mới";
+            this.btn_boqua.UseVisualStyleBackColor = true;
+            this.btn_boqua.Click += new System.EventHandler(this.btn_boqua_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Mức Lương";
+            // 
+            // txt_Luong
+            // 
+            this.txt_Luong.Location = new System.Drawing.Point(114, 92);
+            this.txt_Luong.Name = "txt_Luong";
+            this.txt_Luong.Size = new System.Drawing.Size(238, 20);
+            this.txt_Luong.TabIndex = 24;
+            this.txt_Luong.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Luong_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txt_Luong);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_TenCV);
+            this.groupBox1.Controls.Add(this.txt_MCV);
+            this.groupBox1.Location = new System.Drawing.Point(191, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(410, 132);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập Thông Tin";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(109, 163);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(596, 218);
+            this.groupBox2.TabIndex = 27;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Danh Mục Công Việc";
+            // 
+            // frm_TestKiemTra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_dong);
+            this.Controls.Add(this.btn_boqua);
+            this.Controls.Add(this.btn_excel);
+            this.Controls.Add(this.btn_sua);
+            this.Controls.Add(this.btn_Xoa);
+            this.Controls.Add(this.btn_them);
+            this.Name = "frm_TestKiemTra";
+            this.Text = "Danh Sách Công Việc";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_MCV;
+        private System.Windows.Forms.TextBox txt_TenCV;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_dong;
+        private System.Windows.Forms.Button btn_them;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_sua;
+        private System.Windows.Forms.Button btn_excel;
+        private System.Windows.Forms.Button btn_boqua;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Luong;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.SaveFileDialog dlgSave;
+    }
+}
+
---
+++ TestKiemTra/TestKiemTra/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace TestKiemTra
+{
+    public partial class frm_TestKiemTra : Form
+    {
+        Database db = new Database();
+        public frm_TestKiemTra()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_them_Click(object sender, EventArgs e)
+        {
+            if(txt_MCV.Text.Trim() == "")
+            {
+                MessageBox.Show("Hãy Nhập Mã Công Việc", "Thông Báo");
+                txt_MCV.Focus();
+            }
+            else if (txt_TenCV.Text.Trim() == "")
+            {
+                MessageBox.Show("Hãy Nhập Tên Công Việc", "Thông Báo");
+                txt_TenCV.Focus();
+            }
+            else if (txt_Luong.Text.Trim() == "")
+            {
+                MessageBox.Show("Hãy Nhập Mức Lương", "Thông Báo");
+                txt_Luong.Focus();
+            }
+            else
+            {
+                string sql = " select * from CongViec where MCV = '" + txt_MCV.Text + "' ";
+                DataTable table = new DataTable();
+                table = db.LoadDuLieu(sql);
+                if(table.Rows.Count > 0)
+                {
+                    MessageBox.Show("Mã Công Việc Này Đã Tồn Tại");
+                    txt_MCV.Focus();
+                    return;
+                }
+                string sql1 = " insert into CongViec VALUES ('" + txt_MCV.Text + "',N'" + txt_TenCV.Text + "','" + txt_Luong.Text + "')";
+                db.RunSQL123(sql1);
+                Fill();
+                MessageBox.Show("Thêm Thành Công");
+                ClearData();
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Fill();
+            txt_MCV.Enabled = false;
+            txt_MCV.Text = dataGridView1.Rows[0].Cells[0].Value.ToString();
+            txt_TenCV.Text = dataGridView1.Rows[0].Cells[1].Value.ToString();
+            txt_Luong.Text = dataGridView1.Rows[0].Cells[2].Value.ToString();
+        }
+        private void Fill()
+        {
+            string sql = "select * from CongViec";
+            
+            dataGridView1.DataSource = db.LoadDuLieu(sql);
+            dataGridView1.Columns[0].HeaderText = "Mã Công Việc";
+            dataGridView1.Columns[1].HeaderText = "Tên Công Việc";
+            dataGridView1.Columns[2].HeaderText = "Mức Lương";
+        }
+        private void ClearData()
+        {
+            txt_MCV.Enabled = true;
+            txt_MCV.Text = "";
+            txt_TenCV.Text = "";
+            txt_Luong.Text = "";
+        }
+
+        private void btn_dong_Click(object sender, EventArgs e)
+        {
+            DialogResult ret =
+                MessageBox.Show("Bạn có muốn thoát không", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+            if(ret == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            txt_MCV.Enabled = false;
+            int t = dataGridView1.CurrentRow.Index;
+            txt_MCV.Text = dataGridView1.Rows[t].Cells[0].Value.ToString();
+            txt_TenCV.Text = dataGridView1.Rows[t].Cells[1].Value.ToString();
+            txt_Luong.Text = dataGridView1.Rows[t].Cells[2].Value.ToString();
+        }
+
+        private void txt_Luong_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btn_excel_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0) //TH có dữ liệu để ghi
+            {
+                //Khai báo và khởi tạo các đối tượng
+                Excel.Application exApp = new Excel.Application();
+                Excel.Workbook exBook = exApp.Workbooks.Add(Excel.XlWBATemplate.xlWBATWorksheet);
+                Excel.Worksheet exSheet = (Excel.Worksheet)exBook.Worksheets[1];
+
+                //Định dạng chung
+                Excel.Range tenCuaHang = (Excel.Range)exSheet.Cells[1, 1];
+                tenCuaHang.Font.Size = 14;
+                tenCuaHang.Font.Bold = true;
+                tenCuaHang.Font.Color = Color.Blue;
+                tenCuaHang.Value = "DANH SÁCH CÔNG VIỆC ";
+
+                Excel.Range dcCuaHang = (Excel.Range)exSheet.Cells[2, 1];
+                dcCuaHang.Font.Size = 14;
+                dcCuaHang.Font.Bold = true;
+                dcCuaHang.Font.Color = Color.Blue;
+                dcCuaHang.Value = "Copyright-Vũ Quang Minh";
+
+                Excel.Range dtCuaHang = (Excel.Range)exSheet.Cells[3, 1];
+                dtCuaHang.Font.Size = 14;
+                dtCuaHang.Font.Bold = true;
+                dtCuaHang.Font.Color = Color.Blue;
+                dtCuaHang.Value = "Điện thoại: 1800 8198";
+
+                //Định dạng tiêu đề bảng
+
+                exSheet.get_Range("A7:D7").Font.Bold = true;
+                exSheet.get_Range("A7:D7").HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+                exSheet.get_Range("A7").Value = "STT";
+                exSheet.get_Range("B7").Value = "Mã Công Việc";
+                exSheet.get_Range("C7").Value = "Tên Công Việc";
+                exSheet.get_Range("C7").ColumnWidth = 20;
+                exSheet.get_Range("D7").Value = "Mức Lương";
+
+                //In dữ liệu
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    exSheet.get_Range("A" + (i + 8).ToString() + ":G" + (i + 8).ToString()).Font.Bold = false;
+                    exSheet.get_Range("A" + (i + 8).ToString()).Value = (i + 1).ToString();
+                    exSheet.get_Range("B" + (i + 8).ToString()).Value =
+                        dataGridView1.Rows[i].Cells[0].Value;
+                    exSheet.get_Range("C" + (i + 8).ToString()).Value = dataGridView1.Rows[i].Cells[1].Value;
+                    exSheet.get_Range("D" + (i + 8).ToString()).Value = dataGridView1.Rows[i].Cells[2].Value;
+                }
+                exSheet.Name = "Danh sách";
+                exBook.Activate(); //Kích hoạt file Excel
+                //Thiết lập các thuộc tính của SaveFileDialog
+                SaveFileDialog dlgSave = new SaveFileDialog();
+                dlgSave.Filter = "Excel Document(*.xlsx)|*.xlsx  |Word Document(*.docx) |*.docx|All files(*.*)|*.*";
+                dlgSave.FilterIndex = 1;
+                dlgSave.AddExtension = true;
+                dlgSave.DefaultExt = ".xlsx";
+                if (dlgSave.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    exBook.SaveAs(dlgSave.FileName.ToString());//Lưu file Excel
+                exApp.Quit();//Thoát khỏi ứng dụng
+            }
+            else
+                MessageBox.Show("Không có danh sách hàng để in");
+        }
+
+        private void btn_boqua_Click(object sender, EventArgs e)
+        {
+            ClearData();
+        }
+
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            if(dataGridView1.CurrentRow.Index == -1)
+            {
+                MessageBox.Show("Hãy chọn một công việc để xóa", "Thông Báo");
+            }
+            else
+            {
+                DialogResult ret =
+                    MessageBox.Show("Bạn có muốn xóa không", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk);
+                if (ret == DialogResult.Yes)
+                {
+                    int t = dataGridView1.CurrentRow.Index;
+                    string sql = " select MCV from NhanVien where MCV = '" + dataGridView1.Rows[t].Cells[0].Value.ToString() + "'";
+                    DataTable tb = new DataTable();
+                    tb = db.LoadDuLieu(sql);
+                    if(tb.Rows.Count > 0)
+                    {
+                        MessageBox.Show("Công việc này đã có nhân viên làm rồi không thể xóa.");
+                        return;
+                    }
+                    sql = "delete from CongViec where MCV = '" + txt_MCV.Text + "'";
+                    db.RunSQL123(sql);
+                    MessageBox.Show("Xóa Thành Công");
+                    Fill();
+                    ClearData();
+                }
+            }
+        }
+
+        private void btn_sua_Click(object sender, EventArgs e)
+        {
+            string sql;
+            int t = dataGridView1.CurrentRow.Index;
+            sql = "update CongViec SET MCV = '" + txt_MCV.Text + "', TenCV = N'" + txt_TenCV.Text + "', MucLuong = '"+txt_Luong.Text+"' where MCV = '"+ dataGridView1.Rows[t].Cells[0].Value.ToString() + "'" ;
+            if (txt_TenCV.Text.Trim() == "")
+            {
+                MessageBox.Show("Hãy Nhập Tên Công Việc", "Thông Báo");
+                txt_TenCV.Focus();
+            }
+            else if (txt_Luong.Text.Trim() == "")
+            {
+                MessageBox.Show("Hãy Nhập Mức Lương", "Thông Báo");
+                txt_Luong.Focus();
+            }
+            else
+            {
+                db.RunSQL123(sql);
+                MessageBox.Show("Sửa Thành Công");
+                Fill();
+                ClearData();
+            }
+        }
+    }
+}
